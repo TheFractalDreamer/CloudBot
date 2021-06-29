72e70b8ae4d79237e1fb0c3926e315e80447f3e1
Fractal Dreamer <thefractaldreamer@gmail.com>
2021-06-28 19:06:43 -0500

    Add .ricflair

--- plugins/dpp.py
+++ plugins/dpp.py
@@ -5,3 +5,7 @@ from cloudbot import hook
 @hook.command("duckdance", autohelp=False)
 def duckdance(message):
     message("https://www.youtube.com/watch?v=8An272FPO6E");
+
+@hook.command("ricflair", autohelp=False)
+def ricflair(message):
+    message("WOOOO! https://media.giphy.com/media/yUI3a7RwLhOFy/giphy.gif");
